840cafe9fff83b47db578d412802d691dec3a5d3
Filip Wieland <filiplamparski@gmail.com>
2013-08-24 15:10:09 +0100

    Cards can now carry an arbitrary Object in the data property. Use wisely

--- CardsUILib/src/com/fima/cardsui/objects/AbstractCard.java
+++ CardsUILib/src/com/fima/cardsui/objects/AbstractCard.java
@@ -5,52 +5,68 @@ import android.view.View;
 
 public abstract class AbstractCard {
 
-    protected int image;
-    protected String description, color, titleColor, desc, title, titlePlay;
-    protected Boolean hasOverflow, isClickable;
-    protected int imageRes;
+	protected int image;
+	protected String description, color, titleColor, desc, title, titlePlay;
+	protected Boolean hasOverflow, isClickable;
+	protected int imageRes;
+	protected Object data;
 
-    public abstract View getView(Context context);
+	public abstract View getView(Context context);
 
-    public abstract View getView(Context context, boolean swipable);
+	public abstract View getView(Context context, boolean swipable);
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getTitlePlay() {
-        return titlePlay;
-    }
+	public String getTitlePlay() {
+		return titlePlay;
+	}
 
-    public String getDesc() {
-        return desc;
-    }
+	public String getDesc() {
+		return desc;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public int getImage() {
-        return image;
-    }
+	public int getImage() {
+		return image;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public String getTitleColor() {
-        return titleColor;
-    }
+	public String getTitleColor() {
+		return titleColor;
+	}
 
-    public Boolean getHasOverflow() {
-        return hasOverflow;
-    }
+	public Boolean getHasOverflow() {
+		return hasOverflow;
+	}
 
-    public Boolean getIsClickable() {
-        return isClickable;
-    }
+	public Boolean getIsClickable() {
+		return isClickable;
+	}
 
-    public int getImageRes() {
-        return imageRes;
-    }
+	public int getImageRes() {
+		return imageRes;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public Object getData() {
+		return data;
+	}
+
+	/**
+	 * @param data
+	 *            the data to set
+	 */
+	public void setData(Object data) {
+		this.data = data;
+	}
 }
